f98d3e6e595e337c0b7f37737cf6c94588d25486
Hussain Alkhateeb <hsoon97@gmail.com>
2022-01-20 20:53:52 +0300

    char added

---
+++ Resident Evil Clone/Assets/Scripts/Camera/FirstCam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstCam : MonoBehaviour
+{
+    public GameObject cameraOne;
+    public GameObject cameraTwo;
+    public bool camOn;
+    public int cameraNumber;
+
+    void Start()
+    {
+        cameraNumber = 1;
+        StartCoroutine(CameraSwitch());
+
+    }
+
+    IEnumerator CameraSwitch() {
+        yield return new WaitForSeconds(5);
+        cameraTwo.SetActive(true);
+        cameraOne.SetActive(false);
+        camOn = true;
+        cameraNumber = 2;
+    }
+}
